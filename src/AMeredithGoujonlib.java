7ae08c7a769f9a7fc2776406cdadc4bcb081fd55
Astor730 <42044772+Astor730@users.noreply.github.com>
2018-10-03 08:18:27 -0400

    isPalidrome function

--- src/AMeredithGoujonlib.java
+++ src/AMeredithGoujonlib.java
@@ -1,2 +1,35 @@
 public class AMeredithGoujonlib {
+    public static String dateStr(String date)
+    {
+        if (date.length()>10)
+        {
+            String output= "Date is too long";
+            return output;
+        }
+        else
+        {
+            String mm = date.substring(0,2);
+            String dd = date.substring(3,5);
+            String yyyy = date.substring(6,date.length());
+            String orgdate = dd + " - " + mm + " - " + yyyy;
+            return orgdate;
+        }
+
+    }
+    public static boolean isPalindrome(String word)
+    {
+        String wordBack = word.substring(word.length()-1,word.length());
+        for (int i = 1; i<word.length();i++)
+        {
+            wordBack = wordBack + word.substring(word.length()-i-1,word.length()-i);
+        }
+        if (word.equalsIgnoreCase(wordBack))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
--- src/TestFile.java
+++ src/TestFile.java
@@ -2,6 +2,7 @@ public class TestFile {
     public static void main(String[] args)
     {
         System.out.println(AMeredithGoujonlib.dateStr("01/08/1999"));
+        System.out.println(AMeredithGoujonlib.isPalindrome("racecar"));
     }
     public static void main(String[] args)
     {
