77b1723b04fb77d00bac40d46a801fd84e8784ed
Astor730 <42044772+Astor730@users.noreply.github.com>
2018-10-03 08:00:49 -0400

    isPalidrome function with error

--- src/TestFile.java
+++ src/TestFile.java
@@ -1,6 +1,10 @@
 public class TestFile {
     public static void main(String[] args)
     {
+        System.out.println(AMeredithGoujonlib.dateStr("01/08/1999"));
+    }
+    public static void main(String[] args)
+    {
         System.out.println(AMeredithGoujonlib.sumUpTo(10));
         System.out.println(AMeredithGoujonlib.multiplicationTable(5,5));
     }
