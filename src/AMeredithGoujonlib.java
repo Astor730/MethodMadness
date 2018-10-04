95d8a70135a5a2abdcc3cdff42bce3294e48ce96
BT_1N3_08 <mister.p730@gmail.com>
2018-10-04 14:49:09 -0400

    Methods

--- src/AMeredithGoujonlib.java
+++ src/AMeredithGoujonlib.java
@@ -1,2 +1,20 @@
 public class AMeredithGoujonlib {
+    public static int sumUpTo (int x)
+    {
+        int sum = 0;
+        for (int i = 0; i <= x; i++)
+        {
+            sum = sum + i;
+        }
+        return sum;
+    }
+    public static void multiplicationTable(int b, int r)
+    {
+        int o = 0;
+        for(int i = 0; i<r;i++)
+        {
+            o= b*i;
+            System.out.println(o);
+        }
+    }
 }
--- src/TestFile.java
+++ src/TestFile.java
@@ -1,2 +1,7 @@
 public class TestFile {
+    public static void main(String[] args)
+    {
+        System.out.println(AMeredithGoujonlib.sumUpTo(10));
+        System.out.println(AMeredithGoujonlib.multiplicationTable(5,5));
+    }
 }
