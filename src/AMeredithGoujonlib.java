67b2bc43926f96673f33808dd97750900037c2ba
BT_1N3_08 <mister.p730@gmail.com>
2018-10-05 14:37:49 -0400

    Comments

--- src/AMeredithGoujonlib.java
+++ src/AMeredithGoujonlib.java
@@ -1,14 +1,14 @@
 public class AMeredithGoujonlib {
-    /**
-     *
-     * @param date
-     * @return
+    /**changes a date in mm/dd/yyyy format to dd - mm - yyyy format
+     * if the string entered is longer than 10 the function does not run
+     * @param date is the parameter entered in mm/dd/yyyy format
+     * @return a string that is in the dd - mm - yyyy format
      */
     public static String dateStr(String date)
     {
         if (date.length()>10)
         {
-            String output= "Date is too long";
+            String output = "Date is too long";
             return output;
         }
         else
@@ -21,6 +21,13 @@ public class AMeredithGoujonlib {
         }
 
     }
+
+    /**Checks to see if a word is a palindrome
+     *
+     * @param word is the word to be checked
+     * @return return a boolean that says if the word is a palindrome or not
+     */
+
     public static boolean isPalindrome(String word)
     {
         String wordBack = word.substring(word.length()-1,word.length());
@@ -37,6 +44,12 @@ public class AMeredithGoujonlib {
             return false;
         }
     }
+
+    /**
+     * This function outputs the sum off all numbers up to the parameter
+     * @param x is the number to add up to
+     * @return returns the sum
+     */
     public static int sumUpTo(int x)
     {
         int o = 0;
@@ -46,6 +59,13 @@ public class AMeredithGoujonlib {
         }
         return o;
     }
+
+    /**
+     * This function prints a multiplication table with products of a number up to another number
+     * @param b the number that is mulitplied
+     * @param r the number that b multiplies up to
+     * @return void
+     */
     public static void multiplicationTable(int b, int r)
     {
         for(int i = 0;i<=r;i++)
@@ -53,6 +73,12 @@ public class AMeredithGoujonlib {
             System.out.println(b*i);
         }
     }
+
+    /**
+     * prints a string of numbers up to a number given
+     * @param num1 is the number that the string prints up to
+     * @return a void
+     */
     public static void fooBarBaz(int num1)
     {
         for(int i = 1; i<=num1;i++)
